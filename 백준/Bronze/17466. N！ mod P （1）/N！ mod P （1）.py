ff47c08312c9b4ab85c92aecee28da46637a7cac
Doa-ddaram <119608093+Doa-ddaram@users.noreply.github.com>
2022-12-06 22:41:27 +0900

    [Bronze II] Title: N! mod P (1), Time: 600 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/17466. N！ mod P （1）/N！ mod P （1）.py
+++ 백준/Bronze/17466. N！ mod P （1）/N！ mod P （1）.py
@@ -2,11 +2,9 @@ import sys                          #17466번 미완성본(모듈로의 시작)
 input = sys.stdin.readline
 n, p = map(int,input().split())
 def modul(n,m):
-  res = n
-  if n == 1:
-    return res
-  for i in range(n-1,0,-1):
-    temp = res % m
-    res = temp * i
-  return temp
+  res = 1
+  for i in range(n,0,-1):
+    res = res * i
+    res = res % p
+  return res
 print(modul(n,p))
