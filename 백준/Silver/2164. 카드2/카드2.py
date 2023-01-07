238b38c8454a198a2560d1fcd1fdaaf03aca2465
Doa-ddaram <119608093+Doa-ddaram@users.noreply.github.com>
2023-01-07 22:32:45 +0900

    [Silver IV] Title: 카드2, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,13 +1,12 @@
+# 2 ** 19 == 524288
 n = int(input())
-from collections import deque
-card_q = deque(range(1,n+1))
-t = 0
-while True:
-  t += 1
-  if len(card_q) == 1:
+for i in range(19):
+  if 2 ** i <= n < 2 ** (i+1):
+    t = 2 ** i
     break
-  card_q.popleft()
-  if len(card_q) == 1:
-    break
-  card_q.append(card_q.popleft())
-print(card_q[0])
+if t == n:
+  print(n)
+elif n == 1:
+  print(1)
+else:  
+  print((n - t) * 2)
